32fd08c3570c8e4c3e120cf787e090264aa5eef3
Rostyslav Holub <kloosprog@gmail.com>
2022-12-13 10:23:19 +0200

    move api key to local properties

--- app/src/main/java/ua/rodev/gifsapp/data/cloud/GifsService.kt
+++ app/src/main/java/ua/rodev/gifsapp/data/cloud/GifsService.kt
@@ -2,6 +2,7 @@ package ua.rodev.gifsapp.data.cloud
 
 import retrofit2.http.GET
 import retrofit2.http.Query
+import ua.rodev.gifsapp.BuildConfig
 import ua.rodev.gifsapp.data.cloud.gifs.GifsCloud
 
 interface GifsService {
@@ -9,7 +10,7 @@ interface GifsService {
     // TODO remove api_key
     @GET("gifs/search?")
     suspend fun gifs(
-        @Query("api_key") apiKey: String = "YGHnKKBGSydS6nSt6WAoUcICWwmgCfvL",
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("q") query: String = "cat",
         @Query("offset") offset: Int,
         @Query("limit") limit: Int,
